9dc17325261c86493365dfd15b75038df234324b
Vanya708090 <115784854+Vanya708090@users.noreply.github.com>
2023-07-11 17:54:27 +0300

    new_file

---
+++ Downloader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+
+
+namespace ComicDownloader
+{
+    class Downloader
+    {
+        public static int num = 1;
+        public static void DownloadAllPage(WebClient Client, string url, string path)
+        {
+            int n = 1;
+            while (true)
+            {
+                try {
+                    while (n < 10)
+                    {
+                        Client.DownloadFile(url + $"0{ n }.jpg", path + $"/{ n }.jpg");
+                        n = n + 1;
+                    }
+                    Client.DownloadFile(url + $"{ n }.jpg", path + $"/{ n }.jpg");
+                    n = n + 1;
+
+                }
+                catch
+                {                  
+                    Console.WriteLine($"Комикс {num-1} загружено {n-1} страниц: ");
+                    ZipFile.CreateFromDirectory(path, path+".cbz");
+                    Directory.Delete(path, true);
+                    break;
+                }
+
+                
+            }
+            
+        }
+        public static string CreateDirectory(string path)
+        {
+            path = path + $"{ num }";
+            Directory.CreateDirectory(path);
+            num++;
+            return path;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.IO.Compression;
+
+namespace ComicDownloader
+{
+    class Program
+    {
+        public static void Metod(int n, string url, string path, WebClient Client1)
+        {
+            path = Downloader.CreateDirectory(path);
+            Downloader.DownloadAllPage(Client1, url + $"{n}/", path);
+        }
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int count = Convert.ToInt32(Console.ReadLine());
+            string url1 = "http://img1.unicomics.com/comics/ultimate-spider-man/ultimate-spider-man-00";
+            string url2 = "http://img1.unicomics.com/comics/ultimate-spider-man/ultimate-spider-man-0";
+            string url3 = "http://img1.unicomics.com/comics/ultimate-spider-man/ultimate-spider-man-";
+            WebClient Client1 = new WebClient();
+            while (n<=count)
+            {
+                string path1 = "C:/Users/Computer/Desktop/комикс/";
+                if (n < 10)
+                 {
+                    Metod(n, url1, path1, Client1);
+                 }
+                else if (n < 100 && n > 9)
+                {
+                    Metod(n, url2, path1, Client1);
+                }
+                else
+                {
+                    Metod(n, url3, path1, Client1);
+                }
+                n++;
+            }
+            Console.WriteLine("Загрузка всех комиксов закончена. ");
+        }
+
+    }
+    }
